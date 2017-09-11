aaab11b91b162d5c277aece70dd0bd307591de86
shojo1 <shojo1@umbc.edu>
2017-09-11 12:05:11 -0400

    original file, python script, result file

---
+++ host2dnsmasq.py
@@ -0,0 +1,9 @@
+f = open('oriversionofblacklist.txt', 'r')
+w = open('dnsmasqversionofblacklist.txt','w')
+lines = f.readlines()
+for line in lines:
+    if(line[0] == '0'):
+        tmp = line.split(' ')
+        w.write('\"/'+ tmp[1].replace('\n', '/') + tmp[0] + '\"\n')
+f.close
+w.close
