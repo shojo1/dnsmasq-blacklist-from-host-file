7185f18ade56dc3d83a9db7874bb3a069f9a1825
shojo1 <shojo1@umbc.edu>
2017-09-11 12:18:39 -0400

    fixed dnsmasq syntax

--- host2dnsmasq.py
+++ host2dnsmasq.py
@@ -4,6 +4,6 @@ lines = f.readlines()
 for line in lines:
     if(line[0] == '0'):
         tmp = line.split(' ')
-        w.write('\"/'+ tmp[1].replace('\n', '/') + tmp[0] + '\"\n')
+        w.write('address=\"/'+ tmp[1].replace('\n', '/') + tmp[0] + '\"\n')
 f.close
 w.close
